8fb2694bbbb54f32e92f9d66c6a8f00a6fefd756
艾海涛 <aihaitao@aihaitaos-macbook-air.local>
2015-11-30 21:50:57 +0800

    save

---
+++ gloveiPhone/gloveiPhone/TweetTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TweetTableViewController.h
+//  gloveiPhone
+//
+//  Created by 艾海涛 on 30/11/2015.
+//  Copyright © 2015 geilove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetTableViewController : UITableViewController
+
+@end
