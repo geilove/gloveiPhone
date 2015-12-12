2912114289079cc402ca708f0a4d5bab62c0e556
艾海涛 <aihaitao@aihaitaos-macbook-air.local>
2015-12-12 08:00:00 +0800

    qui大神指点

---
+++ gloveiPhone/gloveiPhone/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  gloveiPhone
+//
+//  Created by 艾海涛 on 2/12/2015.
+//  Copyright © 2015 geilove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarViewController : UITabBarController
+
+@end
---
+++ gloveiPhone/gloveiPhone/TabBarViewController.m
@@ -0,0 +1,46 @@
+//
+//  TabBarViewController.m
+//  gloveiPhone
+//
+//  Created by 艾海涛 on 2/12/2015.
+//  Copyright © 2015 geilove. All rights reserved.
+//
+
+#import "TabBarViewController.h"
+#import "AFHTTPRequestOperationManager.h"
+
+@interface TabBarViewController ()
+
+@end
+
+@implementation TabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"TabBar");
+    
+    
+    
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
